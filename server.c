6a39200537acb5396283792b779704b9581cafdd
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2020-01-09 17:59:35 -0500

    Make server an MPI process

--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <margo.h>
+#include <mpi.h>
 #include "alpha-server.h"
 #include "beta-server.h"
 #include "gamma-server.h"
@@ -8,15 +9,28 @@
 
 int main(int argc, char** argv)
 {
+
+    MPI_Init(&argc, &argv);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    char filename[100];
+    sprintf(filename, "server_%d", rank);
+    FILE *fp = fopen(filename, "w");
+
     margo_instance_id mid = margo_init("ofi+verbs", MARGO_SERVER_MODE, 0, 0);
     assert(mid);
 
     hg_addr_t my_address;
     margo_addr_self(mid, &my_address);
     char addr_str[128];
+    char * temp;
+    char actual_addr[128];
     size_t addr_str_size = 128;
     margo_addr_to_string(mid, addr_str, &addr_str_size, my_address);
-    printf("Server running at address %s, with provider id 42\n", addr_str);
+    temp = addr_str + 17;
+    sprintf(actual_addr, "ofi+verbs%s",temp);
+    fprintf(fp, "%s", actual_addr);
+    fflush(fp);
 
     alpha_provider_register(mid, 42, ALPHA_ABT_POOL_DEFAULT, ALPHA_PROVIDER_IGNORE);
     beta_provider_register(mid, 42, BETA_ABT_POOL_DEFAULT, BETA_PROVIDER_IGNORE);
@@ -32,6 +46,10 @@ int main(int argc, char** argv)
     margo_enable_remote_shutdown(mid);
 
     margo_wait_for_finalize(mid);
+
+    fclose(fp);
+
+    MPI_Finalize();
 
     return 0;
 }
