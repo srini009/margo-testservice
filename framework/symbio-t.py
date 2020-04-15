2f1c89de5322288be91959e52f56a08c174d1148
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2020-04-15 16:20:45 -0400

    Generate legal JSON

--- framework/symbio-t.py
+++ framework/symbio-t.py
@@ -36,22 +36,28 @@ class OperationTree:
 		self.second = second
 		self.third = third
 
-	def traverseTree(self, accessPattern=None):
-		print '{'
-		print '"val": ' + '"'+str(self.microservice_function) + '"'
+	def traverseTree(self, accessPattern=None, isFirstChild=True):
+		requestStructure = ""
+                if isFirstChild: 
+			requestStructure += '{'
+		else:
+			requestStructure += ',{'
+		
+		requestStructure += '"val": ' + '"'+str(self.microservice_function) + '"'
 		if accessPattern != None:
-			print ',' + '"accessPattern": ' + '"'+ str(accessPattern) + '"'
+			requestStructure += ',' + '"accessPattern": ' + '"'+ str(accessPattern) + '"'
 
 		if self.first != None or self.second != None or self.third != None:
-			print ',' + '"children": ['
+			requestStructure += ',' + '"children": ['
 			if self.first != None:
-				self.first[0].traverseTree(self.first[1])
+				requestStructure += self.first[0].traverseTree(self.first[1])
 			if self.second != None:
-				self.second[0].traverseTree(self.second[1])
+				requestStructure += self.second[0].traverseTree(self.second[1], False)
 			if self.third != None:
-				self.third[0].traverseTree(self.third[1])
-			print ']'
-		print "},"
+				requestStructure += self.third[0].traverseTree(self.third[1], False)
+			requestStructure += ']'
+		requestStructure += "}"
+		return requestStructure
 		
 
 
@@ -61,9 +67,11 @@ class OperationType:
 		self.opTree = tree
 
 class Service:
-	def __init__(self):
+	def __init__(self, name):
 		self.microservices = list()
+		self.name = name
 		self.opTypes = set()
+		self.legal_boilerplate = "/*\n  * (C) 2015 The University of Chicago\n  *\n  * See COPYRIGHT in top-level directory.\n*/\n"
 	
 	def addMicroservice(self, microservice):
 		self.microservices.append(microservice)
@@ -71,18 +79,29 @@ class Service:
 	def addOperationType(self, op):
 		self.opTypes.add(op)
 
+	def generateClientHeader(self):
+		filename = self.name + str("_client.h")
+		f = open(filename, "w")
+		f.write(self.legal_boilerplate)
+		for op in self.opTypes:
+			f.write("static char[] " + op.name + " = \"" + str(op.opTree.traverseTree()) + "\";")
+		f.flush()
+		f.close()
+	
+
 
 def main():
-	a = OperationTree(NetworkMicroservice.functions[0], None, None, None)
-	b = OperationTree(ComputeMicroservice.functions[0], (a, AccessPattern.Fixed), (a, AccessPattern.Dynamic), None)
-	c = OperationTree(StorageMicroservice.functions[0], (b, AccessPattern.Dynamic), None, None)
+	a = OperationTree(NetworkMicroservice.functions[0])
+	b = OperationTree(ComputeMicroservice.functions[0], (a, AccessPattern.Fixed), (a, AccessPattern.Dynamic))
+	c = OperationTree(StorageMicroservice.functions[0], (b, AccessPattern.Dynamic))
 
 	op1 = OperationType("op1", c)
-	s = Service()
+	s = Service("dummy")
 	s.addMicroservice(NetworkMicroservice(2))
 	s.addMicroservice(ComputeMicroservice(1))
 	s.addMicroservice(StorageMicroservice(1))
 	s.addOperationType(op1)
+	s.generateClientHeader()
 
 main()
 
