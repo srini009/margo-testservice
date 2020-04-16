d5c173887a223124f235641a2e30b9028f5ff6f7
Srinivasan Ramesh <ramesh.srinivasan09@gmail.com>
2020-04-15 20:54:51 -0400

    Add legal C code for server header generation

--- framework/symbio-t.py
+++ framework/symbio-t.py
@@ -4,26 +4,40 @@
 # See COPYRIGHT in top-level directory.
 
 from enum import Enum
+
+class Microservice(Enum):
+	Network = "network"
+	Memory = "memory"
+	Compute = "compute"
+	Storage = "storage"
 	
 class NetworkMicroservice:
 	functions = ["network_do_work"]
+
 	def __init__(self, num_providers):
 		self.num_providers = num_providers
+		self.microservice_type = Microservice.Network
 	
 class MemoryMicroservice:
 	functions = ["memory_do_work"]
+
 	def __init__(self, num_providers):
 		self.num_providers = num_providers
+		self.microservice_type = Microservice.Memory
 
 class ComputeMicroservice:
 	functions = ["compute_do_work"]
+
 	def __init__(self, num_providers):
 		self.num_providers = num_providers
+		self.microservice_type = Microservice.Compute
 
 class StorageMicroservice:
 	functions = ["storage_do_work"]
+
 	def __init__(self, num_providers):
 		self.num_providers = num_providers
+		self.microservice_type = Microservice.Storage
 
 class AccessPattern(Enum):
 	Fixed = 1
@@ -83,13 +97,29 @@ class Service:
 		filename = self.name + str("_client.h")
 		f = open(filename, "w")
 		f.write(self.legal_boilerplate)
+		#Note to self: Use enums to represent service ops and implement a getter function that takes an enum op and
+		#returns the corresponding JSON string structure for that op
 		for op in self.opTypes:
 			f.write("static char[] " + op.name + " = \"" + str(op.opTree.traverseTree()) + "\";")
 		f.flush()
 		f.close()
+
+	def generateServiceHeader(self):
+		filename = self.name + str("_server.h")
+		f = open(filename, "w")
+		f.write(self.legal_boilerplate)
+
+		service_core_struct = ""
+		for microservice in self.microservices:
+			service_core_struct += "\tint " + microservice.microservice_type.value + "[" + str(microservice.num_providers) + "];\n"
+		service_struct = "typedef struct {\n" + service_core_struct + "} " + self.name +"_service;\n\n"
+		
+		service_init_function = "void initialize_" + self.name + "_service(margo_instance_id mid, " + self.name + "_service d) {}"
+		f.write(service_struct)
+		f.write(service_init_function)
+		f.flush()
+		f.close()
 	
-
-
 def main():
 	a = OperationTree(NetworkMicroservice.functions[0])
 	b = OperationTree(ComputeMicroservice.functions[0], (a, AccessPattern.Fixed), (a, AccessPattern.Dynamic))
@@ -102,6 +132,7 @@ def main():
 	s.addMicroservice(StorageMicroservice(1))
 	s.addOperationType(op1)
 	s.generateClientHeader()
+	s.generateServiceHeader()
 
 main()
 
